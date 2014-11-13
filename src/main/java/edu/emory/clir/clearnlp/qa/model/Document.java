20749884a68a2e4c7b52cf830f94454407217e7d
Tomasz Jurczyk <dzordzyk@gmail.com>
2014-11-13 10:42:04 -0500

    Moved from test + changed Document structure.

--- src/main/java/edu/emory/clir/clearnlp/qa/model/Document.java
+++ src/main/java/edu/emory/clir/clearnlp/qa/model/Document.java
@@ -2,12 +2,14 @@ package edu.emory.clir.clearnlp.qa.model;
 
 import edu.emory.clir.clearnlp.dependency.DEPNode;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class Document
 {
     List<DEPNode> depNodeList = new LinkedList<>();
     List<Argument> attributeList = new LinkedList<>();
-    List<Predicate> predicateList = new LinkedList<>();
+    Map<String, List<Predicate>> predicateList = new HashMap<String, List<Predicate>>();
 }
