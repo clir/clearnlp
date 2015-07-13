46961f65e131ff068c019758f57d402471cb0cc0
jdchoi77 <jinho.choi@emory.edu>
2015-07-13 06:27:04 -0400

    Release: 3.2.0.

--- src/main/scripts/rsync.sh
+++ src/main/scripts/rsync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 jar cf clearnlp.jar$1 edu
-rsync -avc clearnlp.jar$1 jdchoi@ainos.mathcs.emory.edu:/home/jdchoi/lib
-#rsync -avc clearnlp.jar choi@lab0z.mathcs.emory.edu:/home/choi/lib
-#scp choi@lab0z.mathcs.emory.edu:/home/choi/lib/clearnlp.jar jdchoi@ainos.mathcs.emory.edu:/home/jdchoi/lib/clearnlp.jar
+#rsync -avc clearnlp.jar$1 jdchoi@ainos.mathcs.emory.edu:/home/jdchoi/lib
+rsync -avc clearnlp.jar choi@lab0z.mathcs.emory.edu:/home/choi/lib
+scp choi@lab0z.mathcs.emory.edu:/home/choi/lib/clearnlp.jar jdchoi@ainos.mathcs.emory.edu:/home/jdchoi/lib/clearnlp.jar
